25f59b1399dc83546a97e02ede21d151c80701c8
earlyann <119711479+earlyann@users.noreply.github.com>
2023-03-13 15:38:08 -0500

    Form work nearly done

---
+++ app.py
@@ -0,0 +1,89 @@
+from flask import Flask, render_template, request, redirect, url_for, make_response
+from forms import ExerciseForm,NewWorkoutForm, NewUserForm
+from flask_sqlalchemy import SQLAlchemy
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'lift123$'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5433/workout_log'
+
+
+db = SQLAlchemy(app)
+
+Base = automap_base()
+
+# Wrap this code inside app.app_context()
+with app.app_context():
+    Base.prepare(db.engine, reflect=True)
+
+    Movement = Base.classes.movement
+    User = Base.classes.users
+    Exercise = Base.classes.exercise
+
+
+# Defining the index route, including a list all the available routes.
+@app.route("/")
+def home():
+    with app.app_context():
+        response = make_response(f"LACEY'S WORKOUT LOG /new-workout or /new-workout/<username> or /new-user")
+        response.headers = [('Content-Type', 'text/plain')]
+        return response
+
+@app.route('/new-workout', methods=['GET', 'POST'])
+def new_workout():
+    with app.app_context():
+        form = NewWorkoutForm()
+        if request.method == 'POST' and form.validate():
+            session = Session(db.engine)
+            user = session.query(User).filter_by(username=form.username.data).first()
+            if not user:
+                flash('Invalid username')
+                return redirect(url_for('new_workout'))
+
+            # Loop through the exercises list and add each exercise to the database
+            for exercise_form in form.exercises:
+                movement = session.query(Movement).filter_by(name=exercise_form.movement.data).first()
+                if not movement:
+                    movement = Movement(name=exercise_form.movement.data,
+                                        description='',
+                                        primary_muscle_group='')
+                    session.add(movement)
+                    session.commit()
+                exercise = Exercise(user_id=user.user_id,
+                                    movement_id=movement.movement_id,
+                                    sets=exercise_form.sets.data,
+                                    reps=exercise_form.reps.data,
+                                    weight=exercise_form.weight.data,
+                                    time=exercise_form.time.data,
+                                    date=form.date.data)
+                session.add(exercise)
+                session.commit()
+            return redirect(url_for('new_workout'))
+        return render_template('new_workout.html', form=form)
+        
+
+@app.route('/new-user', methods=['GET', 'POST'])
+def new_user():
+    with app.app_context():
+        form = NewUserForm()
+        if request.method == 'POST' and form.validate():
+            session = Session(db.engine)
+            user = User(username=form.username.data,
+                        email=form.email.data)
+            session.add(user)
+            session.commit()
+            return redirect(url_for('new_workout'))
+        return render_template('new_user.html', form=form)
+
+if __name__ == '__main__':
+    with app.app_context():
+        # Move this line inside a request context
+        db.create_all()
+        
+    app.run(debug=True)
+
---
+++ forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, DateField, FieldList, FormField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+from flask_wtf.file import FileField, FileRequired
+from flask_wtf.recaptcha import RecaptchaField
+from flask_wtf import Form
+from flask_wtf import FlaskForm
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+
+
+class ExerciseForm(FlaskForm):
+    movement = StringField('Movement', validators=[DataRequired(), Length(max=100)])
+    sets = IntegerField('Sets', validators=[DataRequired()])
+    reps = IntegerField('Reps', validators=[DataRequired()])
+    weight = IntegerField('Weight', validators=[DataRequired()])
+    time = StringField('Time', validators=[DataRequired(), Length(max=8)])
+    
+
+class NewWorkoutForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    exercises = FieldList(FormField(ExerciseForm), min_entries=1)
+    date = DateField('Date', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+
+class NewUserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(max=50)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=120)])
+    submit = SubmitField('Create Account')
+
